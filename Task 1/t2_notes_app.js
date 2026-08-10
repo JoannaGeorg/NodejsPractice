33c4e4498f1319d0c4cc493a7014fc7bc29f9f4a
JoannaGeorg <joegeorge231@gmail.com>
2026-08-10 17:38:22 +0400

    Added utils and notes to practice loading in ones files and using them in main.

--- Task 1/t2_notes_app.js
+++ Task 1/t2_notes_app.js
@@ -1,8 +1,16 @@
 //loads the core module for file system 'fs'
-const fs = require('fs')
+//const fs = require('fs')
 
 //create and write or replace.
 //fs.writeFileSync('notes.txt', 'This is new content.')
 
 //append message
-fs.appendFileSync('notes.txt', '\nSome more stuff.\n\tJust a trial.')
+//fs.appendFileSync('notes.txt', '\nSome more stuff.\n\tJust a trial.')
+
+//loading from another file.
+const utils = require("./t3_utils")
+const getNotes = require("./t4_notes")
+
+console.log("main:")
+console.log(utils(3, 5))
+console.log(getNotes())
---
+++ Task 1/t3_utils.js
@@ -0,0 +1,11 @@
+console.log("Hi from t3_utils.js")
+
+//to be able to use something in the main file we need to export it here.
+// const name = 'Andrew'
+
+// module.exports = name
+function add(a, b) {
+  return a + b
+}
+
+module.exports = add
