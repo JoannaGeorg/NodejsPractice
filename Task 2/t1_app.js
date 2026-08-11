91fa7d2916589d132f52b81f24bc62eab5773369
JoannaGeorg <joegeorge231@gmail.com>
2026-08-11 13:39:32 +0400

    Installed npm module 'validator' and used it in t1_app.js

--- Task 2/t1_app.js
+++ Task 2/t1_app.js
@@ -1,4 +1,9 @@
 //using function created in Task 1
 const add = require('../Task 1/t3_utils')
 
+//using validator
+const validator = require('validator')
+
 console.log(add(7, 86))
+
+console.log(validator.isURL('www.urlexmaple.com'))
