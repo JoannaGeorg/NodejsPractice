999861f44625f77cf6710898a38594fd2efa6143
JoannaGeorg <joegeorge231@gmail.com>
2026-08-12 17:55:22 +0400

    Installed yargs to parse through command line inputs.

--- Task 3/ta_app.js
+++ Task 3/ta_app.js
@@ -1,13 +1,39 @@
 const fs = require('fs')
+const yargs = require('yargs')
 
 //getting arguments from command line
 //console.log(process.argv[2])
 
-console.log(process.argv)
+//getting arguments using yargs
+// console.log(process.argv)
+console.log(yargs.argv)
 
-const command = process.argv[2]
-if (command === 'add') {
-  console.log("Adding note.")
-} else if (command === 'remove') {
-  console.log("Removing note.")
-}
+//Customize the version of yargs
+yargs.version('1.1.0')
+
+//createing commands using yargs
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note.',
+  handler: function () {
+    console.log("Adding a new note.")
+  }
+}).command({
+  command: 'remove',
+  describe: 'Removing a note.',
+  handler: function () {
+    console.log("Removing Note.")
+  }
+}).command({
+  command: 'list',
+  describe: 'List the notes.',
+  handler: function () {
+    console.log("Listing notes.")
+  }
+}).command({
+  command: 'read',
+  describe: 'Read a note.',
+  handler: function () {
+    console.log("Reading a Note.")
+  }
+}).argv
