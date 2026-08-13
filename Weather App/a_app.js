b5f52ae41287831a9721f66134bea70b8fc5813e
JoannaGeorg <joegeorge231@gmail.com>
2026-08-13 20:10:50 +0400

    Added position API and got longitude and latitude of a place

--- Weather App/a_app.js
+++ Weather App/a_app.js
@@ -3,7 +3,13 @@ const request = require('request')
 
 const url = "https://api.weatherapi.com/v1/current.json?key=d1360ebeaaad424ba12144626261308&q=37.8267,-122.4233"
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.current.temp_c)
+request({ url: url, json: true }, (error, response) => {
+  console.log(`The current temperature is ${response.body.current.temp_c} degrees Celsius.\nThere is ${response.body.current.chance_of_rain}% chance of rain.`)
+})
+
+const positionURL = "https://api.positionstack.com/v1/forward?access_key=97706ed08033b324d2a6757b17b1cdb6&query=Washington"
+
+request({ url: positionURL, json: true }, (error, response) => {
+  console.log(response.body.data[0].latitude)
+  console.log(response.body.data[0].longitude)
 })
