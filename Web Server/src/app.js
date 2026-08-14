972b4431ed06db87a69f83e8c0f7d96a8f57a030
JoannaGeorg <joegeorge231@gmail.com>
2026-08-14 22:01:42 +0400

    Set up HandelBars 'hbs' with express for dynamic web pages.

--- Web Server/src/app.js
+++ Web Server/src/app.js
@@ -1,28 +1,41 @@
 const express = require('express')
 const path = require('path')
+const { title } = require('process')
 
 //call the express function to start the application
 const app = express()
 
-//to use the static html file
+//creating the path to the html files.
 const publicDirPath = path.join(__dirname, '../public')
+
+//To set up Handlebars with express for dynamic sites.
+app.set('view engine', 'hbs')
+
+//to use the static html file
 app.use(express.static(publicDirPath))
 
 //to select what should be shown to the person on the page (<partial_url>, (request, response) => {} )
-//NOTE: The following code will not run with the use of the above static page.
-// app.get('', (req, res) => {
-//   res.send('Hello!')
-// })
 
-app.get('/help', (req, res) => {
-  res.send('Hello! This is the Help Page.')
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Joe'
+  })
 })
 
 app.get('/about', (req, res) => {
-  res.send('The About Page.')
+  res.render('about', {
+    title: 'About Page',
+  })
 })
 
-app.get('/bye', (req, res) => {
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'pls send help. desperate.'
+  })
+})
+
+app.get('/weather', (req, res) => {
   res.send('Good Bye.')
 })
 
